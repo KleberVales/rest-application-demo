9eab5b59fb2d36ba256c450772732b74d4bf5d3c
Kleber Vales <klebervales.dev@gmail.com>
2025-08-12 11:22:46 -0300

    First commit

---
+++ src/main/java/com/example/restapplicationdemo/RestApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapplicationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplicationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplicationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restapplicationdemo/RestApplicationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapplicationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
